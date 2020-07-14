5960caf42b21f828f942d88382c187b5aefa1a5c
Jason Parson <jasonparson@bellsouth.net>
2020-07-14 14:34:51 -0500

    created solution

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        
+        public bool IsAPalindrone(string word) 
+        {
+         
+            string val = word.ToLower();
+
+            return val.SequenceEqual(val.Reverse());
+
+            
+            
+        
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,29 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData( "racecar", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("Civic", true)]
+        [InlineData("horse", false)]
+        [InlineData("Radar", true)]
 
+        public void FirstTest(string val, bool expected)
+        {   //Arrange
+            var wordSmith = new WordSmith();
+
+            //Act
+            bool actual = wordSmith.IsAPalindrone(val);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
+
+  
 }
